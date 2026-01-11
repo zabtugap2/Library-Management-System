648f0109907319b65a31a9c38175cb9c46d6c12a
Zabrenson <zabtugap2@gmail.com>
2026-01-11 16:15:49 +0800

    okay

---
+++ LibraryManagementSystem/LOGIN_HANDLER/LoggedInUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+        public static class LoggedInUser
+        {
+            public static int UserID { get; set; }
+            public static string UserName { get; set; }
+            public static string Role { get; set; }
+            public static string ProfileImagePath { get; set; }
+        }
+    }
+
